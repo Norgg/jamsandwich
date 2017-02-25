07616ea6f39c87993e2362ad265aad816305eef1
John Turner <git@norgg.org>
2017-02-25 15:17:14 +0000

    Bread spawning

---
+++ Assets/SpawnBread.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBread : MonoBehaviour {
+	public GameObject breadFab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+		if (Random.value < 0.01f) {
+			GameObject.Instantiate(breadFab, transform.position, Quaternion.identity);
+		}
+	}
+}
