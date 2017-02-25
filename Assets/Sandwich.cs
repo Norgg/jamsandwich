af73217c8c7bba17290eab5d1bde5e03b9ba59ab
John Turner <git@norgg.org>
2017-02-25 15:06:47 +0000

    Sandwich making.

---
+++ Assets/Sandwich.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sandwich : MonoBehaviour {
+	public GameObject sandwichFab;
+	bool collidedAlready = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetCollided() {
+		collidedAlready = true;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.CompareTag("Bread")) {
+			if (!collidedAlready) {
+				GameObject.Instantiate(sandwichFab, transform.position, Quaternion.identity);
+				GameObject.Destroy(gameObject);
+				GameObject.Destroy(collision.gameObject);
+				collision.gameObject.GetComponent<Sandwich>().SetCollided();
+			}
+		}
+	}
+}
