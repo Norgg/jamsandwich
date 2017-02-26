b7c1fb75e1c39936b917426cf590dfd1aa3a5a58
John Turner <git@norgg.org>
2017-02-26 13:51:27 +0000

    Fix bread gravity and allow grabbing held sandwiches.

--- Assets/PlatingArea.cs
+++ Assets/PlatingArea.cs
@@ -12,7 +12,7 @@ public class PlatingArea : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other) {
         print("Triggered");
-		if (other.CompareTag("Sandwich") && !other.GetComponent<Carriable>().IsBeingCarried()) {
+		if (other.CompareTag("Sandwich")) {
 			sandwiches.Add(other.gameObject);
             Destroy(other.gameObject.GetComponent<Carriable>());
 		}
