b72364cc53ebf3b3e7694201e251d8e589128650
John Turner <git@norgg.org>
2017-02-26 12:58:23 +0000

    Start of jam enforcement.

--- Assets/MakeSandwich.cs
+++ Assets/MakeSandwich.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class MakeSandwich : MonoBehaviour {
 	public GameObject sandwichFab;
 	bool collidedAlready = false;
+	AudioSource splat;
 
 	// Use this for initialization
 	void Start () {
+		splat = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -21,16 +23,29 @@ public class MakeSandwich : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D collision) {
 		if (collision.gameObject.CompareTag("Bread") && !collidedAlready) {
-            var beingCarried = gameObject.GetComponent<Carriable>().IsBeingCarried();
-            var otherBeingCarried = collision.gameObject.GetComponent<Carriable>().IsBeingCarried();
-            if(!beingCarried && !otherBeingCarried)
-            {
-                var sanFab = GameObject.Instantiate(sandwichFab, transform.position, Quaternion.identity);
-                sanFab.GetComponent<Rigidbody2D>().angularVelocity = 200;
-                GameObject.Destroy(gameObject);
-                GameObject.Destroy(collision.gameObject);
-                collision.gameObject.GetComponent<MakeSandwich>().SetCollided();
-            }
+			Jammable jam1 = GetComponent<Jammable>();
+			Jammable jam2 = collision.transform.GetComponent<Jammable>();
+			if (jam1 != null && jam2 != null && jam1.HasJam() && jam2.HasJam()) {				
+				SuccessSandwich(collision.gameObject);
+			} else {
+				FailSandwich(collision.gameObject);
+			}
+		}
+	}
+
+	void FailSandwich(GameObject other) {
+	}
+
+	void SuccessSandwich(GameObject other) {
+		var beingCarried = gameObject.GetComponent<Carriable>().IsBeingCarried();
+		var otherBeingCarried = other.GetComponent<Carriable>().IsBeingCarried();
+		if(!beingCarried && !otherBeingCarried)
+		{
+			var sanFab = GameObject.Instantiate(sandwichFab, transform.position, Quaternion.identity);
+			sanFab.GetComponent<Rigidbody2D>().angularVelocity = 200;
+			GameObject.Destroy(gameObject);
+			GameObject.Destroy(other);
+			gameObject.GetComponent<MakeSandwich>().SetCollided();
 		}
 	}
 }
